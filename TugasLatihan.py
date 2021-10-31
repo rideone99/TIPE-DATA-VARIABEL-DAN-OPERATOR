67c339370e31a2d95cfad5859be91335470dbb5b
rideone99 <ridwanahm1999@gmail.com>
2021-10-31 20:32:24 +0700

    first main

---
+++ TugasLatihan.py
@@ -0,0 +1,8 @@
+import math
+r = float(input("Masukan Jari-jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t= ",format(luas,'.2f'))
+print ("Keliling Lingkaran \t= ",format(keliling,'.2f'))
